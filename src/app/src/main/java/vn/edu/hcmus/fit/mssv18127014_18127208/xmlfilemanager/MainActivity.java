ce4a158a8e82286cc214bfb616dfb8262157e8dc
Nam Huynh Nhat <nnhuynh306@gmail.com>
2021-05-27 08:29:01 +0700

    initialize project

---
+++ src/app/src/main/java/vn/edu/hcmus/fit/mssv18127014_18127208/xmlfilemanager/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.hcmus.fit.mssv18127014_18127208.xmlfilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
