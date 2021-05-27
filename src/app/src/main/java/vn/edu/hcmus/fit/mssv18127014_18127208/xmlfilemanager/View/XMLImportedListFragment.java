cded62ada1a3952b1391748d9accaf7c23bb7b07
Nam Huynh Nhat <nnhuynh306@gmail.com>
2021-05-27 10:26:49 +0700

    + create main layout

---
+++ src/app/src/main/java/vn/edu/hcmus/fit/mssv18127014_18127208/xmlfilemanager/View/XMLImportedListFragment.java
@@ -0,0 +1,21 @@
+package vn.edu.hcmus.fit.mssv18127014_18127208.xmlfilemanager.View;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+import vn.edu.hcmus.fit.mssv18127014_18127208.xmlfilemanager.R;
+
+public class XMLImportedListFragment extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull @org.jetbrains.annotations.NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
